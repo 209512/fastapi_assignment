f9c3c2e917d6a9b8d2f9a7cacbd3599e099f0078
209512 <209512mon@gmail.com>
2025-08-13 18:51:48 +0900

    Requirement 1: User-created api

---
+++ app/schemas/users.py
@@ -0,0 +1,19 @@
+# app/schemas/users.py
+
+from enum import Enum
+from pydantic import BaseModel, Field
+
+# gender field: enum
+class GenderEnum(str, Enum):
+    male = "male"
+    female = "female"
+
+# Request Body 검증 모델
+class UserCreate(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    age: int = Field(gt=0, le=120)
+    gender: GenderEnum
+
+# 유저 생성 응답 모델
+class UserCreateResponse(BaseModel):
+    id: int
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # main.py
 
 from typing import Annotated
-
 from fastapi import FastAPI
-
 from app.models.users import UserModel
-
+from app.schemas.users import UserCreate, UserCreateResponse
 
 app = FastAPI()
 
 UserModel.create_dummy() # API 테스트를 위한 더미를 생성하는 메서드 입니다.
 
+# 1. 유저 생성 API
+@app.post("/user/create", response_model=UserCreateResponse)
+def create_user(user: UserCreate):
+    '''
+    클라이언트 - username, age, gender 받아
+    pydantic(usercreate)로 유효성 검증 후 usermodel에 저장,
+    생성된 유저 Id 반환
+    '''
+    new_user = UserModel.create(
+        username=user.username,
+        age=user.age,
+        gender=user.gender,
+    )
+    return {'id': new_user.id}
 
 if __name__ == '__main__':
     import uvicorn
