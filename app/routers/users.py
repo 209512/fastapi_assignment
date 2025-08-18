57486f4ff221d58d01e884c499ea5dbce5014eaa
209512 <209512mon@gmail.com>
2025-08-19 00:00:10 +0900

    Requirements 4. Login API

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,10 @@
 # app/routers/users.py
 
-from fastapi import APIRouter, HTTPException, Path, Query
+from fastapi import APIRouter, HTTPException, Path, Query, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
-
+from datetime import datetime, timezone
+from app.utils.jwt import create_access_token
 from app.models.users import UserModel
 from app.schemas.users import (
     UserCreate, UserCreateResponse, UserResponse,
@@ -27,3 +29,16 @@ def get_all_users():
     if not users:
         raise HTTPException(status_code=404, detail="No users found")
     return users
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = UserModel.authenticate(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+
+    user.last_login = datetime.now(timezone.utc)
+    token = create_access_token(data={"user_id": user.id})
+    return {"access_token": token, "token_type": "bearer"}
