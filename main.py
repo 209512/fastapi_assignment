f825837b2dd74675f3204671d67eb8c5e02c7a5f
209512 <209512mon@gmail.com>
2025-08-20 14:18:16 +0900

    11. Create FastAPI App and Register Router, Ready to Run uvicorn

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 # main.py
 
 from fastapi import FastAPI
-from app.models.users import UserModel
-from app.models.movies import MovieModel
 from app.routers import users, movies
 
 app = FastAPI()
 
-UserModel.create_dummy()
-MovieModel.create_dummy()
+app.include_router(users.router)
+app.include_router(movies.router)
 
-app.include_router(users.router, tags=["users"])
-app.include_router(movies.router, tags=["movies"])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
