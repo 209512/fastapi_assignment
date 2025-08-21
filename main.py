f8d8bee13ee017de1b88ebd2b5d1b09e406b5702
209512 <209512mon@gmail.com>
2025-08-21 18:25:18 +0900

    5. Add review_router, like_router to FastAPI app

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # main.py
 
 from fastapi import FastAPI
-from app.routers import users, movies
+from app.routers import users, movies, reviews, likes
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(movies.router)
+app.include_router(reviews.router)
+app.include_router(likes.router)
 
 if __name__ == "__main__":
     import uvicorn
